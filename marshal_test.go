4f0c5f547397e653af98884e71088d165b3c15ce
VirtualTam <virtualtam@flibidi.net>
2024-11-05 17:03:10 +0100

    opml: marshal and unmarshal the 'directory' spec example
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- marshal_test.go
+++ marshal_test.go
@@ -34,6 +34,74 @@ var (
 			},
 		},
 	}
+
+	specDocumentDirectory = Document{
+		XMLName: xml.Name{
+			Local: "opml",
+		},
+		Version: "2.0",
+		Head: Head{
+			Title:           "scriptingNewsDirectory.opml",
+			DateCreated:     mustParseTimeGMT("Thu, 13 Oct 2005 15:34:07 GMT"),
+			DateModified:    mustParseTimeGMT("Tue, 25 Oct 2005 21:33:57 GMT"),
+			OwnerName:       "Dave Winer",
+			OwnerEmail:      "dwiner@yahoo.com",
+			VertScrollState: 1,
+			WindowTop:       105,
+			WindowLeft:      466,
+			WindowBottom:    386,
+			WindowRight:     964,
+		},
+		Body: Body{
+			Outlines: []Outline{
+				{
+					Text:    "Scripting News sites",
+					Created: mustParseTimeGMT("Sun, 16 Oct 2005 05:56:10 GMT"),
+					Type:    "link",
+					URL:     "http://hosting.opml.org/dave/mySites.opml",
+				},
+				{
+					Text:    "News.Com top 100 OPML",
+					Created: mustParseTimeGMT("Tue, 25 Oct 2005 21:33:28 GMT"),
+					Type:    "link",
+					URL:     "http://news.com.com/html/ne/blogs/CNETNewsBlog100.opml",
+				},
+				{
+					Text:    "BloggerCon III Blogroll",
+					Created: mustParseTimeGMT("Mon, 24 Oct 2005 05:23:52 GMT"),
+					Type:    "link",
+					URL:     "http://static.bloggercon.org/iii/blogroll.opml",
+				},
+				{
+					Text: "TechCrunch reviews",
+					Type: "link",
+					URL:  "http://hosting.opml.org/techcrunch.opml.org/TechCrunch.opml",
+				},
+				{
+					Text: "Tod Maffin's directory of Public Radio podcasts",
+					Type: "link",
+					URL:  "http://todmaffin.com/radio.opml",
+				},
+				{
+					Text: "Adam Curry's iPodder.org directory",
+					Type: "link",
+					URL:  "http://homepage.mac.com/dailysourcecode/DSC/ipodderDirectory.opml",
+				},
+				{
+					Text:    "Memeorandum",
+					Created: mustParseTimeGMT("Thu, 13 Oct 2005 15:19:05 GMT"),
+					Type:    "link",
+					URL:     "http://tech.memeorandum.com/index.opml",
+				},
+				{
+					Text:    "DaveNet archive",
+					Created: mustParseTimeGMT("Wed, 12 Oct 2005 01:39:56 GMT"),
+					Type:    "link",
+					URL:     "http://davenet.opml.org/index.opml",
+				},
+			},
+		},
+	}
 )
 
 func TestMarshalFileSpec(t *testing.T) {
@@ -46,6 +114,11 @@ func TestMarshalFileSpec(t *testing.T) {
 			tname:             "category",
 			document:          specDocumentCategory,
 			referenceFileName: "category.opml",
+		},
+		{
+			tname:             "directory",
+			document:          specDocumentDirectory,
+			referenceFileName: "directory.opml",
 		},
 	}
 
@@ -85,6 +158,11 @@ func TestUnmarshalFileSpec(t *testing.T) {
 			inputFileName: "category.opml",
 			want:          specDocumentCategory,
 		},
+		{
+			tname:         "directory",
+			inputFileName: "directory.opml",
+			want:          specDocumentDirectory,
+		},
 	}
 
 	for _, tc := range cases {
@@ -113,18 +191,44 @@ func mustParseTimeGMT(dateStr string) time.Time {
 func assertDocumentsEqual(t *testing.T, got, want Document) {
 	t.Helper()
 
+	// OPML metadata
 	if got.XMLName.Local != want.XMLName.Local {
 		t.Errorf("want XMLName.Local %q, got %q", want.XMLName.Local, got.XMLName.Local)
 	}
-
 	if got.Version != want.Version {
 		t.Errorf("want Version %q, got %q", want.Version, got.Version)
 	}
 
+	// Head
 	if !got.Head.DateCreated.Equal(want.Head.DateCreated) {
-		t.Errorf("want Head.DateCreated %q, got %q", want.Head.DateCreated, got.Head.DateCreated)
+		t.Errorf("want Head > DateCreated %q, got %q", want.Head.DateCreated, got.Head.DateCreated)
+	}
+	if !got.Head.DateModified.Equal(want.Head.DateModified) {
+		t.Errorf("want Head > DateModified %q, got %q", want.Head.DateModified, got.Head.DateModified)
+	}
+	if got.Head.OwnerName != want.Head.OwnerName {
+		t.Errorf("want Head > OwnerName %q, got %q", want.Head.OwnerName, got.Head.OwnerName)
+	}
+	if got.Head.OwnerEmail != want.Head.OwnerEmail {
+		t.Errorf("want Head > OwnerEmail %q, got %q", want.Head.OwnerEmail, got.Head.OwnerEmail)
+	}
+	if got.Head.VertScrollState != want.Head.VertScrollState {
+		t.Errorf("want Head > VertScrollState %d, got %d", want.Head.VertScrollState, got.Head.VertScrollState)
+	}
+	if got.Head.WindowTop != want.Head.WindowTop {
+		t.Errorf("want Head > WindowTop %d, got %d", want.Head.WindowTop, got.Head.WindowTop)
+	}
+	if got.Head.WindowLeft != want.Head.WindowLeft {
+		t.Errorf("want Head > WindowLeft %d, got %d", want.Head.WindowLeft, got.Head.WindowLeft)
+	}
+	if got.Head.WindowBottom != want.Head.WindowBottom {
+		t.Errorf("want Head > WindowBottom %d, got %d", want.Head.WindowBottom, got.Head.WindowBottom)
+	}
+	if got.Head.WindowRight != want.Head.WindowRight {
+		t.Errorf("want Head > WindowRight %d, got %d", want.Head.WindowRight, got.Head.WindowRight)
 	}
 
+	// Body
 	if len(got.Body.Outlines) != len(want.Body.Outlines) {
 		t.Fatalf("want %d Outlines, got %d", len(want.Body.Outlines), len(got.Body.Outlines))
 	}
@@ -150,6 +254,12 @@ func assertDocumentsEqual(t *testing.T, got, want Document) {
 
 		if !gotOutline.Created.Equal(wantOutline.Created) {
 			t.Errorf("want Outline %d Created %q, got %q", index, wantOutline.Created, gotOutline.Created)
+		}
+		if gotOutline.Type != wantOutline.Type {
+			t.Errorf("want Outline %d Type %q, got %q", index, wantOutline.Type, gotOutline.Type)
+		}
+		if gotOutline.URL != wantOutline.URL {
+			t.Errorf("want Outline %d URL %q, got %q", index, wantOutline.URL, gotOutline.URL)
 		}
 	}
 }
--- opml.go
+++ opml.go
@@ -5,6 +5,7 @@ package opml
 
 import (
 	"encoding/xml"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -19,21 +20,59 @@ type Document struct {
 
 // A Head contains the metadata for the OPML Document.
 type Head struct {
-	Title       string
-	DateCreated time.Time
+	Title           string
+	DateCreated     time.Time
+	DateModified    time.Time
+	OwnerName       string
+	OwnerEmail      string
+	ExpansionStates []int
+	VertScrollState int
+	WindowTop       int
+	WindowLeft      int
+	WindowBottom    int
+	WindowRight     int
 }
 
 type xmlHead struct {
-	Title          string `xml:"title,omitempty"`
-	DateCreatedStr string `xml:"dateCreated,omitempty"`
+	Title              string `xml:"title,omitempty"`
+	DateCreatedStr     string `xml:"dateCreated,omitempty"`
+	DateModifiedStr    string `xml:"dateModified,omitempty"`
+	OwnerName          string `xml:"ownerName,omitempty"`
+	OwnerEmail         string `xml:"ownerEmail,omitempty"`
+	ExpansionStatesStr string `xml:"expansionState,omitempty"`
+	VertScrollState    int    `xml:"vertScrollState,omitempty"`
+	WindowTop          int    `xml:"windowTop,omitempty"`
+	WindowLeft         int    `xml:"windowLeft,omitempty"`
+	WindowBottom       int    `xml:"windowBottom,omitempty"`
+	WindowRight        int    `xml:"windowRight,omitempty"`
 }
 
 func (h *Head) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	dateCreatedStr := formatRFC1123Time(h.DateCreated)
-
 	xmlHead := xmlHead{
-		Title:          h.Title,
-		DateCreatedStr: dateCreatedStr,
+		Title:           h.Title,
+		OwnerName:       h.OwnerName,
+		OwnerEmail:      h.OwnerEmail,
+		VertScrollState: h.VertScrollState,
+		WindowTop:       h.WindowTop,
+		WindowLeft:      h.WindowLeft,
+		WindowBottom:    h.WindowBottom,
+		WindowRight:     h.WindowRight,
+	}
+
+	if !h.DateCreated.IsZero() {
+		xmlHead.DateCreatedStr = formatRFC1123Time(h.DateCreated)
+	}
+	if !h.DateModified.IsZero() {
+		xmlHead.DateModifiedStr = formatRFC1123Time(h.DateModified)
+	}
+
+	if len(h.ExpansionStates) > 0 {
+		var statesStr []string
+		for _, state := range h.ExpansionStates {
+			statesStr = append(statesStr, strconv.Itoa(state))
+		}
+
+		xmlHead.ExpansionStatesStr = strings.Join(statesStr, ", ")
 	}
 
 	return e.EncodeElement(xmlHead, start)
@@ -45,13 +84,54 @@ func (h *Head) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 		return err
 	}
 
-	dateCreated, err := parseRFC1123Time(xmlHead.DateCreatedStr)
-	if err != nil {
-		return err
+	if xmlHead.DateCreatedStr != "" {
+		dateCreated, err := parseRFC1123Time(xmlHead.DateCreatedStr)
+		if err != nil {
+			return err
+		}
+
+		h.DateCreated = dateCreated
+	}
+
+	if xmlHead.DateModifiedStr != "" {
+		dateModified, err := parseRFC1123Time(xmlHead.DateModifiedStr)
+		if err != nil {
+			return err
+		}
+
+		h.DateModified = dateModified
+	}
+
+	if xmlHead.ExpansionStatesStr != "" {
+		var expansionStates []int
+
+		statesStr := strings.Split(xmlHead.ExpansionStatesStr, ",")
+		for _, stateStr := range statesStr {
+			stateStr = strings.TrimSpace(stateStr)
+
+			if stateStr == "" {
+				continue
+			}
+
+			state, err := strconv.Atoi(stateStr)
+			if err != nil {
+				return err
+			}
+
+			expansionStates = append(expansionStates, state)
+		}
+
+		h.ExpansionStates = expansionStates
 	}
 
 	h.Title = xmlHead.Title
-	h.DateCreated = dateCreated
+	h.OwnerName = xmlHead.OwnerName
+	h.OwnerEmail = xmlHead.OwnerEmail
+	h.VertScrollState = xmlHead.VertScrollState
+	h.WindowTop = xmlHead.WindowTop
+	h.WindowLeft = xmlHead.WindowLeft
+	h.WindowBottom = xmlHead.WindowBottom
+	h.WindowRight = xmlHead.WindowRight
 
 	return nil
 }
@@ -66,12 +146,16 @@ type Outline struct {
 	Text       string
 	Categories []string
 	Created    time.Time
+	Type       string
+	URL        string
 }
 
 type xmlOutline struct {
 	Text       string `xml:"text,attr"`
 	Categories string `xml:"category,attr,omitempty"`
 	CreatedStr string `xml:"created,attr,omitempty"`
+	Type       string `xml:"type,attr,omitempty"`
+	URL        string `xml:"url,attr,omitempty"`
 }
 
 func (o *Outline) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
@@ -81,6 +165,8 @@ func (o *Outline) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 		Text:       o.Text,
 		Categories: strings.Join(o.Categories, ","),
 		CreatedStr: createdStr,
+		Type:       o.Type,
+		URL:        o.URL,
 	}
 
 	return e.EncodeElement(xmlOutline, start)
@@ -92,16 +178,22 @@ func (o *Outline) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 		return err
 	}
 
-	categories := strings.Split(xmlOutline.Categories, ",")
+	if xmlOutline.Categories != "" {
+		o.Categories = strings.Split(xmlOutline.Categories, ",")
+	}
 
-	created, err := parseRFC1123Time(xmlOutline.CreatedStr)
-	if err != nil {
-		return err
+	if xmlOutline.CreatedStr != "" {
+		created, err := parseRFC1123Time(xmlOutline.CreatedStr)
+		if err != nil {
+			return err
+		}
+
+		o.Created = created
 	}
 
 	o.Text = xmlOutline.Text
-	o.Categories = categories
-	o.Created = created
+	o.Type = xmlOutline.Type
+	o.URL = xmlOutline.URL
 
 	return nil
 }
